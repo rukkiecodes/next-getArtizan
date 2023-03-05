ca30439ccb774bea275f6f16884fc3a5ef934db2
rukkiecodes <rukkiecodes@gmail.com>
2023-03-05 19:16:09 +0100

    design auth pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,15 +4,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    component: () => import('@/layouts/auth/Default.vue'),
     children: [
       {
         path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        name: 'Signup',
+        component: () => import('@/views/auth/Signup.vue'),
+      },
+      {
+        path: '/signin',
+        name: 'Signin',
+        component: () => import('@/views/auth/Signin.vue'),
       },
     ],
   },
