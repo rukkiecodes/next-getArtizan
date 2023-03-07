f13e3fc8a16b189e737e579685c6d83d5e098c8c
rukkiecodes <rukkiecodes@gmail.com>
2023-03-07 10:55:02 +0100

    accept booking

--- src/views/artisan/CurrentUser.vue
+++ src/views/artisan/CurrentUser.vue
@@ -2,7 +2,19 @@
     <v-card flat color="transparent" width="400" max-width="100%" class="mx-auto">
         <v-card-text class="text-center">
             <v-avatar color="indigo" size="80">
-                <v-img v-if="profile?.avatar" :src="profile?.avatar" cover />
+                <v-img v-if="profile?.avatar" :src="profile?.avatar" cover>
+                    <v-dialog v-model="photoDialog" activator="parent" width="700">
+                        <v-card>
+                            <v-toolbar floating density="compact" color="transparent">
+                                <v-spacer />
+                                <v-btn color="grey-darken-3" @click="photoDialog = false" icon size="small">
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </v-toolbar>
+                            <v-img :src="profile?.avatar" cover />
+                        </v-card>
+                    </v-dialog>
+                </v-img>
                 <v-icon v-else size="30">mdi-account</v-icon>
             </v-avatar>
         </v-card-text>
@@ -57,6 +69,7 @@ import { doc, getDoc } from '@firebase/firestore';
 import { onMounted, ref } from 'vue';
 
 const profile = ref(null)
+const photoDialog = ref(null)
 
 const props = defineProps({
     user: {
