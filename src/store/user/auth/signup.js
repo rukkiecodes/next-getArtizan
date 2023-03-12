3c6635aabfdf3cdedaf61c26bc1957f18a8a6042
rukkiecodes <rukkiecodes@gmail.com>
2023-03-13 00:06:31 +0100

    modified auth

--- src/store/user/auth/signup.js
+++ src/store/user/auth/signup.js
@@ -42,8 +42,47 @@ export const useUserSignupStore = defineStore('userSignup', {
             router.push('dashboard/overview')
           })
           .catch((error) => {
-            console.log(error)
             this.loading = false
+
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            if (errorCode == 'auth/email-already-in-use') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Email already in use'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/invalid-email') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Invalid email'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/weak-password') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Weak password'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/operation-not-allowed') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Operation not allowed'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/user-disabled') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'User disabled'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/user-not-found') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'User not found'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/wrong-password') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Wrong password'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/invalid-verification-code') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Invalid verification code'
+              snackbar.snackbarColor = 'red'
+            } else if (errorCode == 'auth/invalid-verification-id') {
+              snackbar.snackbar = true
+              snackbar.snackbarText = 'Invalid verification id'
+              snackbar.snackbarColor = 'red'
+            }
           });
       }
     }
