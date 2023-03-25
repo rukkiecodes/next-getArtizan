463941a2aa28ed7060316c67862538fb88c3f976
rukkiecodes <rukkiecodes@gmail.com>
2023-03-25 22:53:27 +0100

    refactoring

---
+++ src/components/Tier.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-chip size="small" :color="profile.user?.tier == 'not verified' ? 'red' : 'green'" class="text-capitalize">{{
+        profile.user?.tier }}</v-chip>
+</template>
+<script setup>
+import { useProfileStore } from '@/store/profile/profile';
+import { ref } from 'vue';
+const profile = ref(useProfileStore());
+</script>
